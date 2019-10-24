a0c2d695bd48980c64e06b1aaac0d0519bd0d596
louis.liao <buzonliao@gmail.com>
2019-10-24 17:52:54 +0800

    first commit

---
+++ I am poor/I am poorTests/I_am_poorTests.swift
@@ -0,0 +1,34 @@
+//
+//  I_am_poorTests.swift
+//  I am poorTests
+//
+//  Created by 廖政豪 on 2019/10/24.
+//  Copyright © 2019 廖政豪. All rights reserved.
+//
+
+import XCTest
+@testable import I_am_poor
+
+class I_am_poorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
