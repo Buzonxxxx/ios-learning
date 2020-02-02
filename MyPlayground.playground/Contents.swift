9a74c49c0a30d176396f4dab8796ebeef18b1db4
louis.liao <buzonliao@gmail.com>
2020-02-02 23:02:45 +0800

    update

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -41,25 +41,25 @@
 //print(arr)
 
 // [struct]
-//struct Town {
-//    let name: String
-//    var citizens: [String]
-//    var resources: [String: Int]
-//
-//    init(name: String, citizens: [String], resources: [String: Int]) {
-//        self.name = name
-//        self.citizens = citizens
-//        self.resources = resources
-//    }
-//
-//    func fortify(){
-//        print("Defences increased!")
-//    }
-//}
-//
-//var anotherTown = Town(name: "Louisland", citizens: ["Louis", "Anney"], resources: ["Mondy": 100, "Water": 1000])
-//anotherTown.fortify()
-//print(anotherTown.name)
+struct Town {
+    let name: String
+    var citizens: [String]
+    var resources: [String: Int]
+
+    init(name: String, citizens: [String], resources: [String: Int]) {
+        self.name = name
+        self.citizens = citizens
+        self.resources = resources
+    }
+
+    func fortify(){
+        print("Defences increased!")
+    }
+}
+
+var anotherTown = Town(name: "Louisland", citizens: ["Louis", "Anney"], resources: ["Mondy": 100, "Water": 1000])
+anotherTown.fortify()
+print(anotherTown.name)
 
 
 
--- OptionalDemo/OptionalDemo/main.swift
+++ OptionalDemo/OptionalDemo/main.swift
@@ -1,11 +1,34 @@
+// [Force unwrapping]
+//let myoptional: String?
+//myoptional = "Louis"
+//let text: String = myoptional!
+//print(text)
+
+// [check for nil value]
+//let myOptional: String?
+//myOptional = nil
+//if myOptional != nil {
+//    let text = myOptional!
+//    print(text)
+//} else {
+//    print("myoptional was found to be nil")
+//}
+
+// [optional binding]
+//let myOptional: String?
+//myOptional = "Louis"
+//if let safeOptional = myOptional {
+//    let text = safeOptional
+//    print(text)
+//} else {
+//    print("myoptional was found to be nil")
+//}
+
 // [nil coalescing]
-
-let myOptional1: String?
-
-myOptional1 = nil
-
-let name = myOptional1 ?? "Louis"
-print(name)
+//let myOptional: String?
+//myOptional = nil
+//let text = myOptional ?? "I'm default value"
+//print(text)
 
 
 // [Optional Chaining]
